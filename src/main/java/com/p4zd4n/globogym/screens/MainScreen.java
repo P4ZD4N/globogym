7ce02e5593ff829c25b9a6fbe424cbe5d9e64c33
Wiktor <wiko700@gmail.com>
2024-06-06 00:05:14 +0200

    Added logo to MainScreen

--- src/main/java/com/p4zd4n/globogym/screens/MainScreen.java
+++ src/main/java/com/p4zd4n/globogym/screens/MainScreen.java
@@ -3,12 +3,17 @@ package com.p4zd4n.globogym.screens;
 import com.p4zd4n.globogym.Main;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class MainScreen {
 
     private Main main;
+
+    private Image logo;
+    private ImageView logoView;
 
     private Button loginButton;
     private Button registerButton;
@@ -26,6 +31,11 @@ public class MainScreen {
         vBox.setPadding(new Insets(10));
         vBox.setSpacing(10);
 
+        logo = new Image(getClass().getResource("/img/logo-no-background.png").toString());
+        logoView = new ImageView(logo);
+        logoView.setFitHeight(96);
+        logoView.setFitWidth(300);
+
         loginButton = new Button("Login");
         loginButton.setOnAction(e -> main.showLoginScreen());
 
@@ -35,7 +45,7 @@ public class MainScreen {
         exitButton = new Button("Exit");
         exitButton.setOnAction(e -> main.exit());
 
-        vBox.getChildren().addAll(loginButton, registerButton, exitButton);
+        vBox.getChildren().addAll(logoView, loginButton, registerButton, exitButton);
 
         return vBox;
     }
