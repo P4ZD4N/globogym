f4508b1bafe3b9596f900262f802db2145952ddb
Wiktor <wiko700@gmail.com>
2024-06-07 02:39:58 +0200

    Added Manager entity

---
+++ src/main/java/com/p4zd4n/globogym/entity/Manager.java
@@ -0,0 +1,10 @@
+package com.p4zd4n.globogym.entity;
+
+import java.time.LocalDate;
+
+public class Manager extends Employee {
+
+    public Manager(String username, String email, String password, String firstName, String lastName, LocalDate birthDate, Double salary) {
+        super(username, email, password, firstName, lastName, birthDate, salary);
+    }
+}
