5187b00191225f7ffab40c4af0c9c265f92ab7eb
Wiktor <wiko700@gmail.com>
2024-06-10 22:51:47 +0200

    Added User private field assigned in constructor, improved view

--- src/main/java/com/p4zd4n/globogym/screens/ClubMemberDashboardScreen.java
+++ src/main/java/com/p4zd4n/globogym/screens/ClubMemberDashboardScreen.java
@@ -2,44 +2,30 @@ package com.p4zd4n.globogym.screens;
 
 import com.p4zd4n.globogym.Main;
 import com.p4zd4n.globogym.entity.User;
+import com.p4zd4n.globogym.panes.TopBorderPane;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 public class ClubMemberDashboardScreen {
 
     private Main main;
 
+    private User user;
+
     private BorderPane borderPane;
-    private HBox topContainer;
 
-    private Image logo;
-    private ImageView logoView;
-
-    public ClubMemberDashboardScreen(Main main) {
+    public ClubMemberDashboardScreen(Main main, User user) {
 
         this.main = main;
+        this.user = user;
     }
 
     public Pane getView() {
 
         borderPane = new BorderPane();
         borderPane.setPadding(new Insets(10, 10, 10, 10));
-
-        logo = new Image(getClass().getResource("/img/logo-no-background.png").toString());
-        logoView = new ImageView(logo);
-        logoView.setFitHeight(96);
-        logoView.setFitWidth(300);
-
-        topContainer = new HBox();
-        topContainer.getChildren().add(logoView);
-        topContainer.setAlignment(Pos.CENTER);
-
-        borderPane.setTop(topContainer);
+        borderPane.setTop(new TopBorderPane(main, user));
 
         return borderPane;
     }
