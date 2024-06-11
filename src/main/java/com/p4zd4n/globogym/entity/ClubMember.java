b1118230de101d377d46629fb00eb7e6567e8efd
Wiktor <wiko700@gmail.com>
2024-06-12 01:06:10 +0200

    Added private fields: membershipCard and paymentsHistory, methods: increaseBalance(), reduceBalance(), updatePaymentsHistory()

--- src/main/java/com/p4zd4n/globogym/entity/ClubMember.java
+++ src/main/java/com/p4zd4n/globogym/entity/ClubMember.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,9 +13,31 @@ public class ClubMember extends User {
 
     private static final long serialVersionUID = -4495183180571699662L;
 
+    private MembershipCard membershipCard;
+    private List<Payment> paymentsHistory = new ArrayList<>();
     private Double balance = 0D;
 
     public ClubMember(String username, String email, String password, String firstName, String lastName, LocalDate birthDate) {
         super(username, email, password, firstName, lastName, birthDate);
+    }
+
+    public void increaseBalance(Double amount) {
+
+        balance += amount;
+        updatePaymentsHistory(LocalDate.now(), amount);
+        User.serializeUsers();
+    }
+
+    public void reduceBalance(Double amount) {
+
+        balance -= amount;
+        updatePaymentsHistory(LocalDate.now(), -amount);
+        User.serializeUsers();
+    }
+
+    public void updatePaymentsHistory(LocalDate date, Double amount) {
+
+        paymentsHistory.add(new Payment(date, amount));
+        User.serializeUsers();
     }
 }
