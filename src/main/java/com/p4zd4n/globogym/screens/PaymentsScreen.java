fb9f20adaaa0aa96abd8020625f05aaf09343773
Wiktor <wiko700@gmail.com>
2024-06-11 23:29:20 +0200

    Changed all User class references to ClubMember

--- src/main/java/com/p4zd4n/globogym/screens/PaymentsScreen.java
+++ src/main/java/com/p4zd4n/globogym/screens/PaymentsScreen.java
@@ -1,47 +1,46 @@
 package com.p4zd4n.globogym.screens;
 
 import com.p4zd4n.globogym.Main;
+import com.p4zd4n.globogym.entity.ClubMember;
 import com.p4zd4n.globogym.entity.User;
 import com.p4zd4n.globogym.panes.CenterPane;
 import com.p4zd4n.globogym.panes.LeftPane;
 import com.p4zd4n.globogym.panes.TopPane;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
 public class PaymentsScreen {
 
     private Main main;
 
-    private User user;
+    private ClubMember clubMember;
 
     private BorderPane borderPane;
     private CenterPane centerPane;
 
     private Label balanceLabel;
 
-    public PaymentsScreen(Main main, User user) {
+    public PaymentsScreen(Main main, ClubMember clubMember) {
 
         this.main = main;
-        this.user = user;
+        this.clubMember = clubMember;
     }
 
     public Pane getView() {
 
         centerPane = new CenterPane();
 
-        balanceLabel = new Label(user.getBalance() + " zł");
+        balanceLabel = new Label(clubMember.getBalance() + " zł");
 
         centerPane.getChildren().add(balanceLabel);
 
         borderPane = new BorderPane();
         borderPane.setPadding(new Insets(20, 20, 20, 20));
-        borderPane.setTop(new TopPane(main, user));
+        borderPane.setTop(new TopPane(main, clubMember));
         borderPane.setCenter(centerPane);
-        borderPane.setLeft(new LeftPane(main, user));
+        borderPane.setLeft(new LeftPane(main, clubMember));
 
         return borderPane;
     }
