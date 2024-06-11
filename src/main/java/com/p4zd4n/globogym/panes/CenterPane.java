bf916f4473f3e93176b0ec96d3fe91da8bba1c74
Wiktor <wiko700@gmail.com>
2024-06-11 23:13:47 +0200

    Added CenterPane class

---
+++ src/main/java/com/p4zd4n/globogym/panes/CenterPane.java
@@ -0,0 +1,13 @@
+package com.p4zd4n.globogym.panes;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+
+public class CenterPane extends VBox {
+
+    public CenterPane() {
+
+        setAlignment(Pos.CENTER);
+        setSpacing(10);
+    }
+}
