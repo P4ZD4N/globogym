c3c6a14129a45e294ec9ac9fabbe5b3cdd121235
Wiktor <wiko700@gmail.com>
2024-06-11 23:27:55 +0200

    Added lombok annotations, serialVersionUID field and balance field

--- src/main/java/com/p4zd4n/globogym/entity/ClubMember.java
+++ src/main/java/com/p4zd4n/globogym/entity/ClubMember.java
@@ -1,8 +1,17 @@
 package com.p4zd4n.globogym.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class ClubMember extends User {
+
+    private static final long serialVersionUID = -4495183180571699662L;
+
+    private Double balance = 0D;
 
     public ClubMember(String username, String email, String password, String firstName, String lastName, LocalDate birthDate) {
         super(username, email, password, firstName, lastName, birthDate);
