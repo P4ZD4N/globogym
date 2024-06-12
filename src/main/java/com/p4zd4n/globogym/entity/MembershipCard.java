e8fc261c9b3c352fb145a455028a62667d4f4c1d
Wiktor <wiko700@gmail.com>
2024-06-12 15:27:38 +0200

    Added implementation of serializable interface, added serialVersionUID, price fields and, added renew, findByUsername, serializeMembershipCards, deserializeMembershipCards methods and getter, setter for price field

--- src/main/java/com/p4zd4n/globogym/entity/MembershipCard.java
+++ src/main/java/com/p4zd4n/globogym/entity/MembershipCard.java
@@ -4,20 +4,24 @@ import com.p4zd4n.globogym.enums.MembershipCardStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Setter
-public class MembershipCard {
+public class MembershipCard implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = -8490544777140165825L;
     private static List<MembershipCard> membershipCards = new ArrayList<>();
+    private static Double price = 150D;
     private static Long counter = 1L;
 
     private Long id;
     private ClubMember owner;
-    private Double price = 150D;
     private LocalDate purchaseDate;
     private LocalDate expirationDate;
     private MembershipCardStatus membershipCardStatus;
@@ -38,11 +42,62 @@ public class MembershipCard {
         membershipCards.add(this);
     }
 
+    public void renew() {
+        membershipCardStatus = MembershipCardStatus.ACTIVE;
+        expirationDate = expirationDate.plusMonths(1);
+    }
+
+    public static MembershipCard findByUsername(String username) {
+
+        Optional<MembershipCard> foundCard = membershipCards.stream()
+                .filter(membershipCard -> membershipCard.getOwner().getUsername().equals(username))
+                .findFirst();
+
+        return foundCard.orElse(null);
+    }
+
+    public static void serializeMembershipCards() {
+        try (FileOutputStream fileOut = new FileOutputStream("membershipCards.bin");
+             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+
+            objectOut.writeObject(membershipCards);
+
+            System.out.println("Membership cards successfully saved to membershipCards.bin");
+        } catch (IOException e) {
+            System.err.println("Error with writing membership cards to membershipCards.bin");
+            e.printStackTrace();
+        }
+    }
+
+    public static void deserializeMembershipCards() {
+
+        try (FileInputStream fileIn = new FileInputStream("membershipCards.bin");
+             ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
+
+            List<MembershipCard> membershipCards = (List<MembershipCard>) objectIn.readObject();
+
+            MembershipCard.setMembershipCards(membershipCards);
+
+            System.out.println("Membership cards successfully read from users.bin");
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error with reading membership cards from users.bin");
+            e.printStackTrace();
+        }
+    }
+
     public static List<MembershipCard> getMembershipCards() {
         return membershipCards;
     }
 
     public static void setMembershipCards(List<MembershipCard> membershipCards) {
         MembershipCard.membershipCards = membershipCards;
+    }
+
+    public static Double getPrice() {
+        return price;
+    }
+
+    public static void setPrice(Double price) {
+        MembershipCard.price = price;
     }
 }
