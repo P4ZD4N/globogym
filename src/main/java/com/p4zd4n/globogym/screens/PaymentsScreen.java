2ff1adcda3176d3ba61c9a53354a48e4a6849e03
Wiktor <wiko700@gmail.com>
2024-06-12 12:04:30 +0200

    Added balance label and list (history of payments and deposits)

--- src/main/java/com/p4zd4n/globogym/screens/PaymentsScreen.java
+++ src/main/java/com/p4zd4n/globogym/screens/PaymentsScreen.java
@@ -2,14 +2,19 @@ package com.p4zd4n.globogym.screens;
 
 import com.p4zd4n.globogym.Main;
 import com.p4zd4n.globogym.entity.ClubMember;
-import com.p4zd4n.globogym.entity.User;
 import com.p4zd4n.globogym.panes.CenterPane;
 import com.p4zd4n.globogym.panes.LeftPane;
 import com.p4zd4n.globogym.panes.TopPane;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Optional;
 
 public class PaymentsScreen {
 
@@ -22,6 +27,8 @@ public class PaymentsScreen {
 
     private Label balanceLabel;
 
+    private Button depositButton;
+
     public PaymentsScreen(Main main, ClubMember clubMember) {
 
         this.main = main;
@@ -32,9 +39,20 @@ public class PaymentsScreen {
 
         centerPane = new CenterPane();
 
-        balanceLabel = new Label(clubMember.getBalance() + " zł");
+        balanceLabel = new Label("Balance: " + clubMember.getBalance() + " PLN");
 
-        centerPane.getChildren().add(balanceLabel);
+        depositButton = new Button("Deposit");
+        depositButton.setOnAction(e -> showDepositOptionsWindow());
+
+        ObservableList<String> items = FXCollections.observableArrayList();
+        clubMember.getPaymentsHistory().reversed().forEach(payment -> items.add(payment.toString()));
+        ListView<String> listView = new ListView<>(items);
+        listView.setPrefHeight(150);
+
+        ScrollPane scrollPane = new ScrollPane(listView);
+        scrollPane.setFitToWidth(true);
+
+        centerPane.getChildren().addAll(balanceLabel, depositButton, scrollPane);
 
         borderPane = new BorderPane();
         borderPane.setPadding(new Insets(20, 20, 20, 20));
@@ -43,5 +61,32 @@ public class PaymentsScreen {
         borderPane.setLeft(new LeftPane(main, clubMember));
 
         return borderPane;
+    }
+
+    private void showDepositOptionsWindow() {
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Deposit amount");
+        alert.setHeaderText("Select appropriate deposit amount");
+
+        ButtonType button10 = new ButtonType("10");
+        ButtonType button20 = new ButtonType("20");
+        ButtonType button50 = new ButtonType("50");
+        ButtonType button100 = new ButtonType("100");
+        ButtonType button200 = new ButtonType("200");
+
+        alert.getButtonTypes().setAll(button10, button20, button50, button100, button200);
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent()) {
+            switch (result.get().getText()) {
+                case "10" -> clubMember.increaseBalance(10.0);
+                case "20" -> clubMember.increaseBalance(20.0);
+                case "50" -> clubMember.increaseBalance(50.0);
+                case "100" -> clubMember.increaseBalance(100.0);
+                case "200" -> clubMember.increaseBalance(200.0);
+            }
+        }
     }
 }
