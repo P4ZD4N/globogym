baa8d5c0cc80b00e28b908e95a1676e7af8e6eac
Wiktor <wiko700@gmail.com>
2024-06-12 12:01:33 +0200

    Added Payment class

---
+++ src/main/java/com/p4zd4n/globogym/entity/Payment.java
@@ -0,0 +1,22 @@
+package com.p4zd4n.globogym.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Payment implements Serializable {
+
+    private LocalDate date;
+    private Double amount;
+
+    @Override
+    public String toString() {
+        return date + ": " + amount;
+    }
+}
