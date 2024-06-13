f60bb2ca0e561fe1c51c9be59d0b3fc22f740257
Wiktor <wiko700@gmail.com>
2024-06-13 21:26:29 +0200

    Added getAllSpecializations() method

--- src/main/java/com/p4zd4n/globogym/enums/CoachSpecialization.java
+++ src/main/java/com/p4zd4n/globogym/enums/CoachSpecialization.java
@@ -1,5 +1,8 @@
 package com.p4zd4n.globogym.enums;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum CoachSpecialization {
 
     STRENGTH,
@@ -7,5 +10,9 @@ public enum CoachSpecialization {
     ENDURANCE,
     WEIGHT_LOSS,
     REHABILITATION,
-    WOMEN_TRAINING
+    WOMEN_TRAINING;
+
+    public static List<CoachSpecialization> getAllSpecializations() {
+        return Arrays.asList(CoachSpecialization.values());
+    }
 }
