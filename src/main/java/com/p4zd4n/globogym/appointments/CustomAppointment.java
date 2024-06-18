3955078f5ec6d051039a9d3589d79fa6f5764c52
Wiktor <wiko700@gmail.com>
2024-06-18 16:12:23 +0200

    Added CustomAppointment class

---
+++ src/main/java/com/p4zd4n/globogym/appointments/CustomAppointment.java
@@ -0,0 +1,24 @@
+package com.p4zd4n.globogym.appointments;
+
+import com.p4zd4n.globogym.entity.Event;
+import jfxtras.scene.control.agenda.Agenda;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomAppointment extends Agenda.AppointmentImplLocal {
+
+    private Event event;
+
+    public CustomAppointment(Event event) {
+        super();
+
+        setStartLocalDateTime(event.getStartDateTime());
+        setEndLocalDateTime(event.getEndDateTime());
+        setSummary(event.getName());
+        setDescription(event.getName());
+
+        this.event = event;
+    }
+}
