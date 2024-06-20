c41832e629c8d1afc86dfabb6d7fb2e4543eaeaf
Wiktor <wiko700@gmail.com>
2024-06-20 18:00:59 +0200

    Added body of AddUserForm class

--- src/main/java/com/p4zd4n/globogym/forms/AddUserForm.java
+++ src/main/java/com/p4zd4n/globogym/forms/AddUserForm.java
@@ -1,4 +1,31 @@
 package com.p4zd4n.globogym.forms;
 
 public class AddUserForm extends Form {
+
+    public AddUserForm() {
+
+        super();
+
+        clubMemberRadioButton.setToggleGroup(group);
+        clubMemberRadioButton.setSelected(true);
+
+        coachRadioButton.setToggleGroup(group);
+
+        add(clubMemberRadioButton, 0, 0);
+        add(coachRadioButton, 1, 0);
+        add(usernameLabel, 0, 1);
+        add(usernameField, 1, 1);
+        add(emailLabel, 0, 2);
+        add(emailField, 1, 2);
+        add(passwordLabel, 0, 3);
+        add(passwordField, 1, 3);
+        add(firstNameLabel, 0, 4);
+        add(firstNameField, 1, 4);
+        add(lastNameLabel, 0, 5);
+        add(lastNameField, 1, 5);
+        add(birthDateLabel, 0, 6);
+        add(birthDateField, 1, 6);
+        add(activeLabel, 0, 7);
+        add(activeCheckbox, 1, 7);
+    }
 }
