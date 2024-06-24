5b14d728216d62f2e0257dbfc069f72d6740881e
Wiktor <wiko700@gmail.com>
2024-06-24 18:13:52 +0200

    Made class implementation of Validatable interface

--- src/main/java/com/p4zd4n/globogym/screens/RegistrationScreen.java
+++ src/main/java/com/p4zd4n/globogym/screens/RegistrationScreen.java
@@ -4,22 +4,19 @@ import com.p4zd4n.globogym.Main;
 import com.p4zd4n.globogym.entity.ClubMember;
 import com.p4zd4n.globogym.entity.Coach;
 import com.p4zd4n.globogym.entity.User;
-import com.p4zd4n.globogym.forms.Form;
 import com.p4zd4n.globogym.forms.RegistrationForm;
+import com.p4zd4n.globogym.util.Validatable;
+import com.p4zd4n.globogym.util.Validation;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import lombok.Getter;
 
-import java.time.LocalDate;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class RegistrationScreen {
+@Getter
+public class RegistrationScreen implements Validatable {
 
     private Main main;
 
@@ -33,8 +30,7 @@ public class RegistrationScreen {
     private Button registerButton;
     private Button backButton;
 
-    private Pattern pattern;
-    private Matcher matcher;
+    private Validation validation;
 
     public RegistrationScreen(Main main) {
 
@@ -76,17 +72,22 @@ public class RegistrationScreen {
         borderPane.setCenter(form);
         borderPane.setBottom(bottomContainer);
 
+        validation = new Validation(form, this);
+
         return borderPane;
     }
 
     private void registerUser() {
 
-        if (!areAllFieldsFilled()) {
+        boolean areAllFieldsFilled = validation.areAllFieldsFilled();
+        boolean isDataValid = validation.isDataValid();
+
+        if (!areAllFieldsFilled) {
             errorLabel.setText("Not all fields have been filled!");
             return;
         }
 
-        if (!isDataValid()) {
+        if (!isDataValid) {
             return;
         }
 
@@ -115,137 +116,5 @@ public class RegistrationScreen {
         main.showMainScreen();
     }
 
-    private boolean areAllFieldsFilled() {
 
-        return form.getUsernameField().getText() != null &&
-                form.getEmailField().getText() != null &&
-                form.getPasswordField().getText() != null &&
-                form.getFirstNameField().getText() != null &&
-                form.getLastNameField().getText() != null &&
-                form.getBirthDateField().getValue() != null;
-    }
-
-    private boolean isDataValid() {
-
-        return isUsernameValid() &&
-                isEmailValid() &&
-                isPasswordValid() &&
-                isFirstNameAndLastNameValid() &&
-                isBirthDateValid();
-    }
-
-    private boolean isUsernameValid() {
-
-        Optional<User> existingUser = User.getUsers().stream()
-                                                    .filter(user -> user.getUsername().equals(form.getUsernameField().getText()))
-                                                    .findFirst();
-
-        if (existingUser.isPresent()) {
-            errorLabel.setText("User with this username already exists!");
-            return false;
-        }
-
-        if (form.getUsernameField().getText().length() <= 4) {
-            errorLabel.setText("Username must consist of at least 5 characters!");
-            return false;
-        }
-
-        String usernamePattern = "[^a-zA-Z0-9]";
-        pattern = Pattern.compile(usernamePattern);
-        matcher = pattern.matcher(form.getUsernameField().getText());
-        boolean doesUsernameContainSpecialCharacters = matcher.find();
-
-        if (doesUsernameContainSpecialCharacters) {
-            errorLabel.setText("Username shouldn't contain special characters!");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean isEmailValid() {
-
-        Optional<User> existingUser = User.getUsers().stream()
-                                                    .filter(user -> user.getEmail().equals(form.getEmailField().getText()))
-                                                    .findFirst();
-
-        if (existingUser.isPresent()) {
-            errorLabel.setText("User with this email already exists!");
-            return false;
-        }
-
-        String emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-        pattern = Pattern.compile(emailPattern);
-        matcher = pattern.matcher(form.getEmailField().getText());
-        boolean isEmailValid = matcher.find();
-
-        if (!isEmailValid) {
-            errorLabel.setText("Invalid email!");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean isPasswordValid() {
-
-        String passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).+$";
-        pattern = Pattern.compile(passwordPattern);
-        matcher = pattern.matcher(form.getPasswordField().getText());
-        boolean isPasswordValid = matcher.find();
-
-        if (!isPasswordValid) {
-            errorLabel.setText("Password must consist of lowercase and uppercase letters, numbers and special characters!");
-            return false;
-        }
-
-        if (form.getPasswordField().getText().length() < 8) {
-            errorLabel.setText("Password must be at least 8 characters long!");
-            return false;
-        }
-
-        if (!form.getPasswordField().getText().equals(form.getConfirmPasswordField().getText())) {
-            errorLabel.setText("Entered passwords are different!");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean isFirstNameAndLastNameValid() {
-
-        String namePattern = "^[A-Z][a-zA-Z]*$";
-        pattern = Pattern.compile(namePattern);
-        matcher = pattern.matcher(form.getFirstNameField().getText());
-        boolean isFirstNameValid = matcher.find();
-
-        if (!isFirstNameValid) {
-            errorLabel.setText("First name must start with a capital letter and consist only of letters!");
-            return false;
-        }
-
-        matcher = pattern.matcher(form.getLastNameField().getText());
-        boolean isLastNameValid = matcher.find();
-
-        if (!isLastNameValid) {
-            errorLabel.setText("Last name must start with a capital letter and consist only of letters!");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean isBirthDateValid() {
-
-        LocalDate birthDate = form.getBirthDateField().getValue();
-        LocalDate todayDate = LocalDate.now();
-        LocalDate thresholdDate = todayDate.minusYears(18);
-
-        if (birthDate.isAfter(thresholdDate)) {
-            errorLabel.setText("You must be at least 18 years old!");
-            return false;
-        }
-
-        return true;
-    }
 }
