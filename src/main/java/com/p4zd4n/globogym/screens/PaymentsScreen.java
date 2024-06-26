be7191a6817859ae9b27472509b3992f59a3a22a
Wiktor <wiko700@gmail.com>
2024-06-26 21:27:33 +0200

    Improved payments appearance

--- src/main/java/com/p4zd4n/globogym/screens/PaymentsScreen.java
+++ src/main/java/com/p4zd4n/globogym/screens/PaymentsScreen.java
@@ -8,13 +8,14 @@ import com.p4zd4n.globogym.panes.TopPane;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-
-import java.time.LocalDate;
-import java.util.Map;
-import java.util.Optional;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class PaymentsScreen {
 
@@ -65,28 +66,40 @@ public class PaymentsScreen {
 
     private void showDepositOptionsWindow() {
 
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Deposit amount");
-        alert.setHeaderText("Select appropriate deposit amount");
+        Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.setTitle("Deposit Amount");
 
-        ButtonType button10 = new ButtonType("10");
-        ButtonType button20 = new ButtonType("20");
-        ButtonType button50 = new ButtonType("50");
-        ButtonType button100 = new ButtonType("100");
-        ButtonType button200 = new ButtonType("200");
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.setPadding(new Insets(10));
+        dialogVbox.getChildren().add(new Label("Select appropriate deposit amount"));
+        dialogVbox.setAlignment(Pos.CENTER);
 
-        alert.getButtonTypes().setAll(button10, button20, button50, button100, button200);
+        Button button10 = createDepositButton("10", 10.0);
+        Button button20 = createDepositButton("20", 20.0);
+        Button button50 = createDepositButton("50", 50.0);
+        Button button100 = createDepositButton("100", 100.0);
+        Button button200 = createDepositButton("200", 200.0);
 
-        Optional<ButtonType> result = alert.showAndWait();
+        dialogVbox.getChildren().addAll(button10, button20, button50, button100, button200);
 
-        if (result.isPresent()) {
-            switch (result.get().getText()) {
-                case "10" -> clubMember.increaseBalance(10.0);
-                case "20" -> clubMember.increaseBalance(20.0);
-                case "50" -> clubMember.increaseBalance(50.0);
-                case "100" -> clubMember.increaseBalance(100.0);
-                case "200" -> clubMember.increaseBalance(200.0);
-            }
-        }
+        Scene dialogScene = new Scene(dialogVbox, 300, 300);
+
+        dialog.setScene(dialogScene);
+        dialog.showAndWait();
+    }
+
+    private Button createDepositButton(String text, double amount) {
+
+        Button button = new Button(text);
+
+        button.setMinWidth(100);
+        button.setOnAction(e -> {
+            clubMember.increaseBalance(amount);
+            ((Stage) button.getScene().getWindow()).close();
+            main.showPaymentsScreen(clubMember);
+        });
+
+        return button;
     }
 }
