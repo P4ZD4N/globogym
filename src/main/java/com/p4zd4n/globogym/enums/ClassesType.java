0ea27ae40db47acb8cd7836e82484710430322ae
Wiktor <wiko700@gmail.com>
2024-06-26 11:30:44 +0200

    Added getByString() method

--- src/main/java/com/p4zd4n/globogym/enums/ClassesType.java
+++ src/main/java/com/p4zd4n/globogym/enums/ClassesType.java
@@ -24,4 +24,17 @@ public enum ClassesType {
     public static List<ClassesType> getAll() {
         return Arrays.asList(ClassesType.values());
     }
+
+    public static ClassesType getByString(String str) {
+
+        return switch (str) {
+            case "Strength" -> ClassesType.STRENGTH;
+            case "Cardio" -> ClassesType.CARDIO;
+            case "Endurance" -> ClassesType.ENDURANCE;
+            case "Weight Loss" -> ClassesType.WEIGHT_LOSS;
+            case "Rehabilitation" -> ClassesType.REHABILITATION;
+            case "Women Training" -> ClassesType.WOMEN_TRAINING;
+            default -> throw new IllegalArgumentException("Unknown EventType: " + str);
+        };
+    }
 }
